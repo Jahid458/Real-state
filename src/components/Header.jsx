cdd6969f48d7cdb85dfc31d4aa71b702787c8f25
Jahid458 <Jahidul898@outlook.com>
2026-02-11 09:54:03 +0600

    Add Authentication using clerk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,55 @@
 import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
 import React from "react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
-  return <header className="bg-slate-200 shadow-md">
-        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link href="/">
-            <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-                <span className="text-slate-500">Reals </span>
-                <span className="text-slate-700">Estate </span>
-            </h1>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Reals </span>
+            <span className="text-slate-700">Estate </span>
+          </h1>
         </Link>
 
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search Here..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
 
-            <input type="text" placeholder="Search Here..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
-
-            <button>
-                <FaSearch  className="text-slate-600"/>
-            </button>
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
 
         <ul className="flex gap-4">
-            <Link href="/">
-                <li className="hidden md:inline text-slate-700 hover:underline">
-                      Home   
-                </li>
-            </Link>
-            <Link href="/about">
-                <li className="hidden md:inline text-slate-700 hover:underline">
-                      About
-                </li>
-            </Link>
-            <Link href="/sign-in">
-                <li className="hidden md:inline text-slate-700 hover:underline">
-                      Sign In   
-                </li>
-            </Link>
+          <Link href="/">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
 
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <Link href="/sign-in">
+              <li className="hidden md:inline text-slate-700 hover:underline">
+                Sign In
+              </li>
+            </Link>
+          </SignedOut>
         </ul>
-        </div>
-    </header>;
+      </div>
+    </header>
+  );
 }
